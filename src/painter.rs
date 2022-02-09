f0195e69a5d90fc60261d26fa9af3e0314c28566
Jens Hausdorf <mail@jens-hausdorf.de>
2022-02-09 13:19:07 +0100

    Add initial version of flood filling

--- src/painter.rs
+++ src/painter.rs
@@ -1,4 +1,4 @@
-use crate::data::{NodeData, RelationData, WayData};
+use crate::data::{ElementData, NodeData, RelationData, WayData};
 use crate::element::canvas::CanvasElement;
 use crate::mapcss::declaration::{
     MapCssDeclarationList, MapCssDeclarationProperty, MapCssDeclarationValueType, ToBooleanValue,
@@ -6,7 +6,8 @@ use crate::mapcss::declaration::{
 };
 use crate::mapcss::parser::IntSize;
 use std::cmp;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use std::convert::TryInto;
 use std::num::NonZeroI64;
 use std::time::Instant;
 
@@ -35,7 +36,6 @@ impl Painter for PngPainter {
         rid_to_relation_data: HashMap<NonZeroI64, RelationData>,
     ) -> String {
         const IMAGE_PART_SIZE: u32 = 256;
-        const IMAGE_PADDING: u32 = IMAGE_PART_SIZE / 4;
         const DRAWN_WAYS: usize = 1_000_000;
 
         let canvas = CanvasElement {};
@@ -79,6 +79,7 @@ impl Painter for PngPainter {
             way_data: &'a WayData,
         }
 
+        info!("Sorting by z-index…");
         let mut z_index_ordered_ways = wid_to_way_data
             .values()
             .map(|way_data| WayWithZIndex {
@@ -95,6 +96,7 @@ impl Painter for PngPainter {
 
         z_index_ordered_ways.sort_by(|a, b| a.z_index.cmp(&b.z_index));
 
+        info!("Rasterizing ways…");
         for way_data in z_index_ordered_ways.iter().map(|x| x.way_data) {
             let way_refs = way_data.refs();
 
@@ -111,6 +113,12 @@ impl Painter for PngPainter {
                 )
                 .to_color()
                 .into();
+
+            let is_closed_way = way_data.has_closed_path();
+
+            let mut pixeled_boundaries = HashSet::new();
+            let mut pixeled_min_x_coordinates: (u32, u32) = (u32::MAX, u32::MAX);
+            let mut pixeled_max_x_coordinates: (u32, u32) = (u32::MIN, u32::MIN);
 
             for ref_node_ids in way_refs[..].windows(2) {
                 if let [node_a_id, node_b_id] = ref_node_ids {
@@ -137,9 +145,141 @@ impl Painter for PngPainter {
                             image_y,
                             image::Rgba([way_color[0], way_color[1], way_color[2], 255]),
                         );
+
+                        if is_closed_way {
+                            pixeled_boundaries.insert((image_x, image_y));
+
+                            // speedup "are we inside the drawn way boundaries?"
+                            if image_x > pixeled_max_x_coordinates.0 {
+                                pixeled_max_x_coordinates = (image_x, image_y);
+                            }
+
+                            if image_x < pixeled_min_x_coordinates.0 {
+                                pixeled_min_x_coordinates = (image_x, image_y);
+                            }
+                        }
                     }
                 } else {
                     unreachable!();
+                }
+            }
+
+            // flood fill the closed way using the algorithm specified here: https://en.wikipedia.org/wiki/Flood_fill#Span_Filling
+            if is_closed_way {
+                fn is_inside(
+                    (mut x, y): (i64, i64),
+                    min_x: i64,
+                    max_x: i64,
+                    pixeled_boundaries: &HashSet<(u32, u32)>,
+                ) -> bool {
+                    if x < min_x {
+                        return false;
+                    }
+
+                    let mut intersected_boundary_times: u8 = 0;
+
+                    loop {
+                        if x > max_x {
+                            break;
+                        }
+                        x += 1;
+
+                        if pixeled_boundaries
+                            .contains(&(x.try_into().unwrap(), y.try_into().unwrap()))
+                        {
+                            intersected_boundary_times += 1;
+                        }
+                    }
+
+                    intersected_boundary_times % 2 != 0
+                }
+
+                fn get_flood_filled_pixels(
+                    (mut x, y): (i64, i64),
+                    min_x: i64,
+                    max_x: i64,
+                    pixeled_boundaries: &HashSet<(u32, u32)>,
+                ) -> Vec<(u32, u32)> {
+                    if !is_inside((x, y), min_x, max_x, pixeled_boundaries) {
+                        warn!("initial value is not inside the boundaries");
+                        return Vec::new();
+                    }
+
+                    info!("Flood filling polynom…");
+
+                    let mut flood_filled_pixels: Vec<(u32, u32)> = Vec::new();
+
+                    let mut stack: Vec<(i64, i64, i64, i64)> = Vec::new();
+
+                    stack.push((x, x, y, 1));
+                    stack.push((x, x, y - 1, -1));
+
+                    while let Some((mut x1, x2, y, dy)) = stack.pop() {
+                        x = x1;
+
+                        debug!("Substep 1, stack size: {}", stack.len());
+
+                        if is_inside((x, y), min_x, max_x, pixeled_boundaries) {
+                            while x > 1 && is_inside((x - 1, y), min_x, max_x, pixeled_boundaries) {
+                                flood_filled_pixels
+                                    .push(((x - 1).try_into().unwrap(), y.try_into().unwrap()));
+
+                                x -= 1;
+                            }
+                        }
+
+                        if x < x1 {
+                            stack.push((x, x1 - 1, y - dy, -dy));
+                        }
+
+                        debug!("Substep 2, stack size: {}", stack.len());
+
+                        while x1 < x2 {
+                            while is_inside((x1, y), min_x, max_x, pixeled_boundaries) {
+                                flood_filled_pixels
+                                    .push((x1.try_into().unwrap(), y.try_into().unwrap()));
+
+                                x1 += 1;
+                            }
+
+                            stack.push((x, x1 - 1, y + dy, dy));
+
+                            if x1 - 1 > x2 {
+                                stack.push((x2 + 1, x1 - 1, y - dy, -dy));
+                            }
+
+                            while x1 < x2 && !is_inside((x1, y), min_x, max_x, pixeled_boundaries) {
+                                x1 += 1;
+                            }
+
+                            x = x1;
+                        }
+
+                        if stack.len() > 15 {
+                            warn!("stack size exploded, not filling polynom");
+                            return Vec::new();
+                        }
+                    }
+
+                    info!("Filled polynom with {} pixels", flood_filled_pixels.len());
+
+                    flood_filled_pixels
+                }
+
+                for flood_filled_pixel in get_flood_filled_pixels(
+                    (
+                        (pixeled_max_x_coordinates.0 - 1).into(),
+                        pixeled_max_x_coordinates.1.into(),
+                    ),
+                    pixeled_min_x_coordinates.1.into(),
+                    pixeled_max_x_coordinates.1.into(),
+                    &pixeled_boundaries,
+                ) {
+                    image_buffer.put_pixel(
+                        flood_filled_pixel.0,
+                        flood_filled_pixel.1,
+                        image::Rgba([way_color[0], way_color[1], way_color[2], 255]),
+                    );
                 }
             }
 
@@ -157,6 +297,9 @@ impl Painter for PngPainter {
             render_duration.as_secs_f32(),
             rendered_ways as f64 / (render_duration.as_nanos() as f64 * 1e-9)
         );
+
+        dbg!(image_height);
+        dbg!(image_width);
 
         let filename = String::from("test.png");
 
