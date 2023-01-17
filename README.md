2a91ba66ed8c416311776b1d681daefd4271dae7
Jens Hausdorf <mail@jens-hausdorf.de>
2023-01-17 22:05:47 +0100

    Update render example... it's been one year of progress! 🥳

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # rosm
+![example image](example2.png)
+(Featuring Liechtenstein, rendered on 17th January 2023)
+
 ![example image](example.png)
 (Featuring Liechtenstein, rendered on 14th February 2022)
--- src/main.rs
+++ src/main.rs
@@ -99,7 +99,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let instant = Instant::now();
     let (nid_to_node_data, wid_to_way_data, rid_to_relation_data) =
-        extractor::extract_data_from_filepath(String::from("saarland-latest.osm.pbf"))?;
+        extractor::extract_data_from_filepath(String::from("liechtenstein-latest.osm.pbf"))?;
 
     print_peak_memory_usage();
     info!(
